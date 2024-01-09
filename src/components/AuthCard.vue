52d6de22992241dff52c7e1b7f869db276150efd
Pels <pelumisunday@gmail.com>
2024-01-09 14:47:11 +0100

    feat: create layout comps

---
+++ src/components/AuthCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({})
+</script>
+
+<template>
+<div class="bg-white rounded-2xl p-6 w-126 max-w-[90vw]">
+  <header>
+    <slot name="header"></slot>
+  </header>
+  <main>
+    <slot name="main"></slot>
+  </main>
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
+</div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <div class="bg-auth bg-no-repeat bg-cover min-h-screen flexCenter">
+    <div class="flexDown">
+      <img src="@/assets/images/logo-full.svg" alt="logo" class="mb-5" />
+      <slot />
+      <ul
+        class="bg-accent-gray-400 flex text-sm font-medium text-primary mt-auto rounded-full py-2 px-3 gap-6 [&_li]:cursor-pointer absolute bottom-5">
+        <li>Privacy & Terms</li>
+        <li>Contact us</li>
+        <li class="flex items-center gap-2"><span>Change region</span><img src="@/assets/icons/arrow-downward.svg" alt="">
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
+
