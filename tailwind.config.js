4d723d4a7031a329b030c3371e0467e649d85ba0
Pels <pelumisunday@gmail.com>
2024-01-09 08:44:23 +0100

    style: custom and extended styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
+const mapleColors = {
+  // Primary
+  'primary': '#1F2937',
+  // Secondary
+  'secondary': '#9CA3AF',
+  // Neutral
+  // Background
+  // Accent
+  'accent-green': '#AEF378',
+  'accent-blue': '#476EF9'
+}
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {...mapleColors}
+    },
+      backgroundImage: {
+        'auth': "url('@/assets/images/Pattern.png')",
+      }
   },
   plugins: [],
 }
+
