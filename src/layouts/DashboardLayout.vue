ca3b38ecd9267aba1bbb047f32453d3426add2b6
Pels <pelumisunday@gmail.com>
2024-01-10 04:56:44 +0100

    feat: create dashboard layout comp

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HeaderNav from '@/components/HeaderNav.vue'
+import SideNav from '@/components/SideNav.vue'
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <header-nav />
+    <hr class="w-full text-accent-gray-200" />
+    <div class="max-container flex my-4 h-[90vh]">
+      <side-nav />
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
+
