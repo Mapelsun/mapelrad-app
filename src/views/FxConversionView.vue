01fceecd3873c1ac0074f922cea37a1ab93a6a98
Pels <pelumisunday@gmail.com>
2024-01-10 06:26:02 +0100

    feat: create fx conversion page

---
+++ src/views/FxConversionView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import DashboardLayout from '@/layouts/DashboardLayout.vue'
+</script>
+
+<template>
+  <dashboard-layout>
+    FX Conversion
+  </dashboard-layout>
+</template>
+
+<style scoped></style>
