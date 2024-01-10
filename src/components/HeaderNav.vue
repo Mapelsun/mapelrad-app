3db8d892f0ea879c259c59187ba91f3955076b57
Pels <pelumisunday@gmail.com>
2024-01-10 04:57:05 +0100

    feat: create dashboard header nav

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <div class="max-container flexBetween">
+    <img src="@/assets/images/logo.svg" alt="logo" class="cursor-pointer" />
+    <div class="flex gap-5">
+      <div class="flex items-center gap-1 cursor-pointer">
+        <img src="@/assets/icons/code.svg" alt="" />
+        <span class="font-semibold text-base">Developer</span>
+        <img src="@/assets/icons/arrow-downward.svg" alt="" />
+      </div>
+      <img src="@/assets/icons/bell.svg" alt="" class="cursor-pointer" />
+      <div class="flex gap-2">
+        <img src="@/assets/images/ellipse.svg" alt="" />
+        <div class="flex flex-col">
+          <span class="font-semibold text-sm">Chidalu Ugwu</span>
+          <span class="font-medium text-xs text-accent-gray-500">Maplerad Technologies</span>
+        </div>
+        <img src="@/assets/icons/arrow-downward.svg" alt="" class="cursor-pointer" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
