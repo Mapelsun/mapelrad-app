070969f16dd8140ac2cb6c37ed12bbd8bb7f8c4e
Pels <pelumisunday@gmail.com>
2024-01-10 16:59:36 +0100

    update: get user fullName from store

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup>
+import { ref, computed } from 'vue'
+import { useGlobalStore } from '@/stores/global'
+const global = useGlobalStore()
+
+const fullName = computed(() => `${global.authUser.first_name} ${global.authUser.last_name}`)
+
+</script>
 
 <template>
   <div class="max-container flexBetween">
@@ -13,7 +20,7 @@
       <div class="flex gap-2">
         <img src="@/assets/images/ellipse.svg" alt="" />
         <div class="flex flex-col">
-          <span class="font-semibold text-sm">Chidalu Ugwu</span>
+          <span class="font-semibold text-sm">{{ fullName || 'John Doe' }}</span>
           <span class="font-medium text-xs text-accent-gray-500">Maplerad Technologies</span>
         </div>
         <img src="@/assets/icons/arrow-downward.svg" alt="" class="cursor-pointer" />
