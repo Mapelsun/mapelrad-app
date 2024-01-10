07aeeaaf031ce113d88cc8d5d395988870e7c488
Pels <pelumisunday@gmail.com>
2024-01-10 04:57:43 +0100

    feat: create dashboard home view comp

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import DashboardLayout from '@/layouts/DashboardLayout.vue'
+</script>
+
+<template>
+  <dashboard-layout>
+    Dashboard
+  </dashboard-layout>
+</template>
+
+<style scoped></style>
