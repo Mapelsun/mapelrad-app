903d5e9b8fb95bc034e7d6dec40efd5d4db73cad
Pels <pelumisunday@gmail.com>
2024-01-10 16:59:21 +0100

    feat: implement otp verification

--- src/views/OtpView.vue
+++ src/views/OtpView.vue
@@ -5,15 +5,29 @@ import AuthLayout from '@/layouts/AuthLayout.vue'
 import AuthCard from '@/components/AuthCard.vue'
 import CusButton from '@/components/CusButton.vue'
 import OtpInput from '@/components/OtpInput.vue'
+import apiServices from '@/utils/api-services.js'
+import { useGlobalStore } from '@/stores/global'
 
+const global = useGlobalStore()
 const router = useRouter()
 
 const otpVal = ref('')
 
-const test = () => {
-  console.log('I was clicked here')
-}
+const handleOtpVerification = async () => {
+  const { email } = global.user
+  const payload = { email, otp: otpVal.value }
 
+  try {
+    const response = await apiServices.verifyOtp(payload)
+    if (response.status && response.message === 'Login successful') {
+      global.setAuthUser(response.data)
+      apiServices.setToken(response.data.token)
+      router.push('/dashboard')
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
 </script>
 
 <template>
@@ -33,7 +47,8 @@ const test = () => {
           <div class="flex flex-col mb-6">
             <otp-input @otp-complete="(otp) => otpVal = otp" />
           </div>
-          <cus-button @click="test" variant="green-bg" custom-classes="mb-4">Submit</cus-button>
+          <cus-button @click="handleOtpVerification" variant="green-bg" custom-classes="mb-4"
+            :disabled="!otpVal">Submit</cus-button>
           <p class="text-secondary text-sm text-center mb-8">
             Didn’t receive any code?
             <span class="text-accent-blue cursor-pointer">Resend</span>
