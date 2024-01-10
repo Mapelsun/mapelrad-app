07f1e3cc4a959aebbcceee84b9409d1b86271fe9
Pels <pelumisunday@gmail.com>
2024-01-10 16:56:46 +0100

    feat: ensure store persists on refresh

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import { useGlobalStore } from './stores/global'
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(router)
 app.use(pinia)
+
+useGlobalStore(pinia)
+useGlobalStore().loadState()
+
 app.mount('#app')
