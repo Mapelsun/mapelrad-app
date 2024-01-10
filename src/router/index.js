be27144123013e8e49e0f0e44972f118d27fc451
Pels <pelumisunday@gmail.com>
2024-01-10 06:25:45 +0100

    temp: add temp comp for all missing routes

--- src/router/index.js
+++ src/router/index.js
@@ -9,26 +9,80 @@ const router = createRouter({
       name: 'Login',
       component: LoginView,
       meta: {
-        title: 'Maplerad - Home'
-      }
+        title: 'Maplerad - Home',
+      },
     },
     {
       path: '/otp-verification',
-      name: 'verify',
+      name: 'Verify',
       component: () => import('../views/OtpView.vue'),
       meta: {
-        title: 'Maplerad - Otp Verification'
-      }
+        title: 'Maplerad - Otp Verification',
+      },
     },
     {
       path: '/dashboard',
-      name: 'home',
-      component: () => import('../views/DashboardView.vue'),
-      meta: {
-        title: 'Maplerad - Home'
-      }
-    }
-  ]
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: () => import('../views/DashboardView.vue'),
+          meta: {
+            title: 'Maplerad - Home',
+          },
+        },
+        {
+          path: 'fx-conversion',
+          name: 'FX Conversion',
+          component: () => import('../views/FxConversionView.vue'),
+          meta: {
+            title: 'Maplerad - FX Conversion',
+          },
+        },
+        {
+          path: 'get-started',
+          name: 'Get Started',
+          component: () => import('../views/DashboardView.vue'),
+          meta: {
+            title: 'Maplerad - Get Started',
+          },
+        },
+        {
+          path: 'cash',
+          name: 'Cash',
+          component: () => import('../views/DashboardView.vue'),
+          meta: {
+            title: 'Maplerad - Cash',
+          },
+        },
+        {
+          path: 'card',
+          name: 'Card',
+          component: () => import('../views/DashboardView.vue'),
+          meta: {
+            title: 'Maplerad - Card',
+          },
+        },
+        {
+          path: 'customers',
+          name: 'Customers',
+          component: () => import('../views/DashboardView.vue'),
+          meta: {
+            title: 'Maplerad - Customers',
+          },
+        },
+        {
+          path: 'send-invite',
+          name: 'Invite People',
+          component: () => import('../views/DashboardView.vue'),
+          meta: {
+            title: 'Maplerad - Invite People',
+          },
+        },
+      ],
+    },
+  ],
 })
 
 export default router
+
