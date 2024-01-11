67a6539ba9cf11de444ad0e3be0e60bcc1806e4b
Pels <pelumisunday@gmail.com>
2024-01-11 03:36:50 +0100

    build: add apexchat as dep

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import { useGlobalStore } from './stores/global'
+import VueApexCharts from "vue3-apexcharts"
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(router)
 app.use(pinia)
+app.use(VueApexCharts)
 
 useGlobalStore(pinia)
 useGlobalStore().loadState()
