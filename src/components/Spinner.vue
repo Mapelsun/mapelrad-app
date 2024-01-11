5264f8bc26d6ae342bf8a83bdfbf7d5be5350b82
Pels <pelumisunday@gmail.com>
2024-01-11 04:42:22 +0100

    feat: create spinner icon for button loading state

---
+++ src/components/Spinner.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+  isLoading: Boolean,
+})
+</script>
+
+<template>
+  <div v-if="isLoading" class="flex items-center justify-center flex-col">
+    <div
+      class="border-[5px] border-solid border-gray-300 border-t-[5px] border-t-accent-gray-700 rounded-full w-6 h-6 animate-spin">
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
